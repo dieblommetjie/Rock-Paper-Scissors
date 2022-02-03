d15238a513962d82f2e462abd53a175544f420d9
dieblommetjie <tarrynvdvyver@outlook.com>
2022-02-03 14:43:34 +0200

    Add

--- rps.js
+++ rps.js
@@ -6,44 +6,44 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection == "rock" && computerSelection == "Rock"){
-        //console.log("Draw! Rock and rock");
+    if (playerSelection == "r" && computerSelection == "Rock"){
+        console.log("Draw! Rock and rock");
         return 3;
     }
-    else if (playerSelection == "rock" && computerSelection == "Paper"){
-        //console.log("You Lose! Paper beats rock");
+    else if (playerSelection == "r" && computerSelection == "Paper"){
+        console.log("You Lose! Paper beats rock");
         return 2;
     }
-    else if (playerSelection == "rock" && computerSelection == "Scissors"){
-        //console.log("You Win! Rock beats scissors");
+    else if (playerSelection == "r" && computerSelection == "Scissors"){
+        console.log("You Win! Rock beats scissors");
         return 1;
     }
-    else if (playerSelection == "paper" && computerSelection == "Rock"){
-        //console.log("You win! Paper beats rock");
+    else if (playerSelection == "p" && computerSelection == "Rock"){
+        console.log("You win! Paper beats rock");
         return 1;
     }
-    else if (playerSelection == "paper" && computerSelection == "Paper"){
-        //console.log("Draw! Paper and paper");
+    else if (playerSelection == "p" && computerSelection == "Paper"){
+        console.log("Draw! Paper and paper");
         return 3;
     }
-    else if (playerSelection == "paper" && computerSelection == "Scissors"){
-        //console.log("You Lose! Scissors beats paper");
+    else if (playerSelection == "p" && computerSelection == "Scissors"){
+        console.log("You Lose! Scissors beats paper");
         return 2;
     }
-    else if (playerSelection == "scissors" && computerSelection == "Rock"){
-        //console.log("You Lose! Rock beats scissors");
+    else if (playerSelection == "s" && computerSelection == "Rock"){
+        console.log("You Lose! Rock beats scissors");
         return 2;
     }
-    else if (playerSelection == "scissors" && computerSelection == "Paper"){
-        //console.log("You Win! Scissors beats paper");
+    else if (playerSelection == "s" && computerSelection == "Paper"){
+        console.log("You Win! Scissors beats paper");
         return 1;
     }
-    else if (playerSelection == "scissors" && computerSelection == "Scissors"){
-        //console.log("Draw! Scissors and scissors");
+    else if (playerSelection == "s" && computerSelection == "Scissors"){
+        console.log("Draw! Scissors and scissors");
         return 3;
     }
     else{
-        //console.log("Invalid input");
+        console.log("Invalid input");
         //let k = prompt("Choose 'Rock', 'Paper' or 'Scissors'");
         //let r = computerPlay();
         //playRound(k, r);
@@ -55,7 +55,7 @@ function game(){
     let player = 0;
     let comp = 0;
     for(let i = 0; i < 5; i++){
-        let k = prompt("Choose 'Rock', 'Paper' or 'Scissors'");
+        let k = prompt("Choose rock (r), paper (p) or scissors (s)");
         let r = computerPlay();
         let ans = playRound(k, r);
 
@@ -65,6 +65,7 @@ function game(){
     }
     
     if (player > comp) return "You win!";
+    else if (comp == player) return "It's a DRAW!";
     else return "You lose!";
 }
 
